67a2bba21fc64f738b0bb945564de82155b82490
nileshyadav123456 <nileshyadav667@gmail.com>
2025-12-24 14:56:44 +0530

    Update Azure backend configuration for dev environment

--- envs/dev/provider.tf
+++ envs/dev/provider.tf
@@ -8,13 +8,13 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "practice-rg-001"
-    storage_account_name = "tfstatestgdev001"
-    container_name       = "tfstate"
+    storage_account_name = "tfstatestgnil001"
+    container_name       = "nilcontainer"
     key                  = "dev.terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "f91191dd-7344-4f56-9504-6b05dc9c3eb5"
+  subscription_id = "22135bdc-d3fd-4095-9a18-2094dab79b13"
 }
